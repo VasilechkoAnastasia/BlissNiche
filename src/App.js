d136698148817df05e6113c4323c7342e54fce5f
Vasil_ys90 <Vasil_ys90@2-2>
2025-05-02 11:52:34 +0300

    щ

--- src/App.js
+++ src/App.js
@@ -162,75 +162,75 @@ const App = () => {
             <div className="catalog-grid">
               <div className="catalog-item">
                 <img src="five-section-katalog1.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">ДИВАН</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog2.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КРІСЛО ДЕКОРАТИВНЕ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog3.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СОФА</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog4.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КОМОД</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog5.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КРІСЛО ВІДПОЧИНКОВЕ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog6.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СТІЛ ПИСЬМОВИЙ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog7.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СТІЛ КУТОВИЙ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog8.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КРІСЛО З ПОДУШКАМИ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog9.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СТІЛ ОБІДНІЙ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog10.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">ДИВАН КОМПАКТНИЙ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog11.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">ТАБУРЕТ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog12.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КРІСЛО КОМПАКТНЕ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog13.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КРІСЛО СТИЛЬНЕ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog14.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СОФА-ЛІЖКО</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog15.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">КРІСЛО ЕЛЕГАНТНЕ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog16.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">ДИВАН ДВОМІСНИЙ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog17.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СТІЛЕЦЬ ЖУРНАЛЬНИЙ</p>
               </div>
               <div className="catalog-item">
                 <img src="five-section-katalog18.png" className="catalog-image" />
-                <p className="catalog-title">НАЗВА НАБОРУ</p>
+                <p className="catalog-title">СТІЛ ЖУРНАЛЬНИЙ</p>
               </div>
             </div>
           </section>
@@ -271,15 +271,17 @@ const App = () => {
           <section className="seven-main-recomendation">
             <p className="title-section-main">ДОДАТКОВІ РЕКОМЕНДАЦІЇ</p>
             <div className="title-strip">
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
+              <span className="title-strip-item">КАРТИНИ</span>
+              <span className="title-strip-item">ЛЮСТРА</span>
+              <span className="title-strip-item">ЛАМПА</span>
+              <span className="title-strip-item">КОРЗИНА ДЛЯ БІЛИЗНИ</span>
+              <span className="title-strip-item">ПЛЕДИ</span>
+              <span className="title-strip-item">МАТРАЦ</span>
+              <span className="title-strip-item">СТЕЛАЖ</span>
+              <span className="title-strip-item">КИЛИМ</span>
+              <span className="title-strip-item">ДЕКОРАТИВНІ РОСЛИНИ</span>
+              <span className="title-strip-item">РУШНИК</span>
+              <span className="title-strip-item">ШАФА</span>
             </div>
             <div className="masonry-grid">
               <div className="image-grid">
@@ -310,15 +312,17 @@ const App = () => {
               </div>
             </div>
             <div className="title-strip">
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
-              <span className="title-strip-item">НАЗВА НАБОРУ</span>
+              <span className="title-strip-item">ШТОРИ</span>
+              <span className="title-strip-item">ДЗЕРКАЛО</span>
+              <span className="title-strip-item">ВАЗА</span>
+              <span className="title-strip-item">ГОДИННИК</span>
+              <span className="title-strip-item">ПОЛИЦІ</span>
+              <span className="title-strip-item">ПОДУШКИ</span>
+              <span className="title-strip-item">СВІЧКИ</span>
+              <span className="title-strip-item">ДЕКОР</span>
+              <span className="title-strip-item">ПОСТІЛЬНА БІЛИЗНА</span>
+              <span className="title-strip-item">КУХОННИЙ ПОСУД</span>
+              <span className="title-strip-item">КУХОННА МИЙКА</span>
             </div>
           </section>
 
@@ -332,7 +336,7 @@ const App = () => {
               <div className="info-card-text">
                 <h3 className="info-card-title">ВАЖЛИВА ІНФОРМАЦІЯ</h3>
                 <p className="info-card-description">
-                  Lorem ipsum dolor sit amet consectetur. Bibendum neque at em integer integer. Tempus a mi in enim dui rhoncus vulputate nulla et. Ornare sed cursus sed viverra.
+                У нашому меблевому магазині деякі продукти можна стилізувати за кольором і матеріалом під ваші особисті вподобання, створюючи унікальний дизайн для вашого дому. Звертайтесь до наших консультантів щоб дізнатися деталі!
                 </p>
               </div>
             </div>
@@ -350,17 +354,25 @@ const App = () => {
         <div className="footer-content">
           <div className="footer-top">
             <div className="footer-column">
-              <h3 className="footer-column-title highlighted">Заголовок підрозділу</h3>
+              <h3 className="footer-column-title highlighted">Головна</h3>
               <ul className="footer-column-list">
-                <li><a href="#link1">Зміст підтеми</a></li>
+                <li><a href="#link1">Все для дому</a></li>
+                <li><a href="#link2">Знайди те, шо шукаєш!</a></li>
+                <li><a href="#link3">Рекомендації</a></li>
+                <li><a href="#link4">Найкращі набори</a></li>
+                <li><a href="#link5">Новинки</a></li>
+                <li><a href="#link6">Каталог</a></li>
+                <li><a href="#link7">Ідеї для оформлення</a></li>
+                <li><a href="#link8">Додаткові рекомендації</a></li>
+                <li><a href="#link9">Важлива інформація</a></li>
+              </ul>
+            </div>
+            <div className="footer-column">
+              <h3 className="footer-column-title">Дизайн</h3>
+              <ul className="footer-column-list">
+                <li><a href="#link1">Створи дизайн своєї мрії</a></li>
                 <li><a href="#link2">Зміст підтеми</a></li>
                 <li><a href="#link3">Зміст підтеми</a></li>
-                <li><a href="#link4">Зміст підтеми</a></li>
-                <li><a href="#link5">Зміст підтеми</a></li>
-                <li><a href="#link6">Зміст підтеми</a></li>
-                <li><a href="#link7">Зміст підтеми</a></li>
-                <li><a href="#link8">Зміст підтеми</a></li>
-                <li><a href="#link9">Зміст підтеми</a></li>
               </ul>
             </div>
             <div className="footer-column">
@@ -369,6 +381,9 @@ const App = () => {
                 <li><a href="#link1">Зміст підтеми</a></li>
                 <li><a href="#link2">Зміст підтеми</a></li>
                 <li><a href="#link3">Зміст підтеми</a></li>
+                <li><a href="#link4">Зміст підтеми</a></li>
+                <li><a href="#link5">Зміст підтеми</a></li>
+                <li><a href="#link6">Зміст підтеми</a></li>
               </ul>
             </div>
             <div className="footer-column">
@@ -377,9 +392,6 @@ const App = () => {
                 <li><a href="#link1">Зміст підтеми</a></li>
                 <li><a href="#link2">Зміст підтеми</a></li>
                 <li><a href="#link3">Зміст підтеми</a></li>
-                <li><a href="#link4">Зміст підтеми</a></li>
-                <li><a href="#link5">Зміст підтеми</a></li>
-                <li><a href="#link6">Зміст підтеми</a></li>
               </ul>
             </div>
             <div className="footer-column">
@@ -408,11 +420,11 @@ const App = () => {
 
             <div className="footer-bottom-right">
               <ul className="footer-bottom-links">
-                <li><a href="#link1">Заголовок підрозділу</a></li>
-                <li><a href="#link2">Заголовок підрозділу</a></li>
-                <li><a href="#link3">Заголовок підрозділу</a></li>
-                <li><a href="#link4">Заголовок підрозділу</a></li>
-                <li><a href="#link5">Заголовок підрозділу</a></li>
+                <li><a href="#link1">Товари</a></li>
+                <li><a href="#link2">Книги</a></li>
+                <li><a href="#link3">Дизайн</a></li>
+                <li><a href="#link4">Місто</a></li>
+                <li><a href="#link5">Вхід</a></li>
               </ul>
             </div>
           </div>
