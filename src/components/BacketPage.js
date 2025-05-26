c5052ed859ba60420864591e509a063102e1f304
Anastasia <stacivasilechko@gmail.com>
2025-05-26 15:37:55 +0300

    lp

---
+++ src/components/BacketPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './BacketPage.css';
+import HeaderMain from './HeaderMain.js';
+import FooterMain from './FooterMain.js';
+
+export function BacketPage() {
+
+    return (
+        <div className="app">
+            <HeaderMain />
+
+
+            <div className="wrap">
+                <main>
+                    <section className="first-backet-section">
+                        
+                    </section>
+
+
+                    <section className="second-backet-section">
+                        
+                    </section>
+                </main>
+
+
+            </div>
+            <FooterMain />
+        </div>
+    );
+};
+export default BacketPage;
+
